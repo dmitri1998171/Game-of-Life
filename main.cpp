22117716c449573525d0642a6dcaf77c4829867b
dmitri1998171 <dmitri1998171@icloud.com>
2021-07-15 13:21:43 +0700

    add print info and rules

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
@@ -17,34 +19,6 @@ enum GAME_STATES {
 using namespace std;
 using namespace sf;
 
-// char currGen[gridStep][gridStep] = {
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "           *            ",
-//     "          **            ",
-//     "    ********            ",
-//     "       ********         ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-//     "                        ",
-// };
-
 class CellClass {
     private: 
         int neighborsCount = 0;
@@ -59,6 +33,9 @@ class CellClass {
         Color deathCellColor = Color::White;
         
     public:
+        Clock clock;
+        Time elapsed = clock.getElapsedTime();
+
     // ==== SFML methods =============================
        
         void drawGrid() {
@@ -94,7 +71,7 @@ class CellClass {
             for(int i = 0; i < gridStep; i++) 
                 for(int j = 0; j < gridStep; j++) {
                     currGen[i][j] = ' ';
-                    cell[i][j].setFillColor(Color::White);
+                    cell[i][j].setFillColor(deathCellColor);
                 }
         }
 
@@ -178,13 +155,52 @@ class CellClass {
             // }
         }
 
+    // ===============================================
+
+        int timer(int seconds) {
+            Time elapsed1;
+
+            elapsed1 = clock.getElapsedTime();
+            if(elapsed1.asSeconds() > 10.0) 
+                return 1;
+
+            return 0;
+        }
+
+        void loadTextFromFile(char *path, char array[]) { 
+            char tmp[255];
+            FILE *file = fopen(path, "r");
+
+            while (fgets(tmp, 255, file) != NULL)
+                strcat(array, tmp);
+                // array.insert(array.length(), tmp);
+                // array.push_back(*tmp);
+
+            fclose(file);
+        }
+
+        // void loadTextFromFile(char *path, string array) { 
+        //     char tmp[255];
+        //     FILE *file = fopen(path, "r");
+        //     while (fgets(tmp, 255, file) != NULL)
+        //         // strcat(array, tmp);
+        //         // array.insert(array.length(), tmp);
+        //         // array.push_back(*tmp);
+        //     fclose(file);
+        // }
+
         void run(RenderWindow* window) {
             bool cycle = true;
             int startCount = 0;
+            int infoTextState = 1, rulesTextState = 0;
+            int fontSize = 24;
+            // string rulesText;
+            // string infoText;
+            char infoText[55];
+            char rulesText[2715];
 
             Font font;
-            Text text;
-            int fontSize = 24;
+            Text text, info_text, rules_text;
             String str = "You win!";
             font.loadFromFile("./CyrilicOld.TTF");
 
@@ -193,6 +209,20 @@ class CellClass {
             text.setString(str);
             text.setCharacterSize(fontSize);
             text.setPosition((WINDOW_SIZE - (str.getSize() * (fontSize / 2))) / 2, (WINDOW_SIZE - (fontSize * 2)) / 2);
+
+            // setlocale(LC_ALL, "Rus");
+            loadTextFromFile("info.txt", infoText);
+            loadTextFromFile("rules.txt", rulesText);
+
+            int tc = 75;
+            info_text.setFont(font);
+            info_text.setFillColor(Color(tc, tc, tc));
+            info_text.setString(infoText);
+
+            rules_text.setFont(font);
+            rules_text.setFillColor(Color(tc, tc, tc));
+            // rules_text.setString(String::fromUtf8(rulesText[0], rulesText[strlen(rulesText)]));
+            rules_text.setString(rulesText);
 
             while(cycle) {
                 Event event;
@@ -203,6 +233,12 @@ class CellClass {
                     }
 
                     if (event.type == sf::Event::KeyPressed) {
+                        if (event.key.code == sf::Keyboard::I) {
+                            infoTextState = 1;
+                        }
+                        if (event.key.code == sf::Keyboard::H) {
+                            rulesTextState = 1;
+                        }
                         if (event.key.code == sf::Keyboard::C) {
                             if(playState == PLAY_STATE || playState == PAUSE_STATE) {
                                 playState = START_STATE;
@@ -252,6 +288,20 @@ class CellClass {
                 for(int i = 0; i < gridStep; i++) 
                     for(int j = 0; j < gridStep; j++) 
                         window->draw(cell[i][j]);
+
+                if(infoTextState) {
+                    window->draw(info_text);
+
+                    if(timer(5))
+                        infoTextState = 0;
+                }
+
+                if(rulesTextState) {
+                    window->draw(rules_text);
+
+                    if(timer(10))
+                        rulesTextState = 0;
+                }
                 
                 window->display();
             }
